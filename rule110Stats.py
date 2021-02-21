12e4b0b0968e009a05647d74384855a2eb778ea5
Shrohan Mohapatra <46818279+ShrohanMohapatra@users.noreply.github.com>
2021-02-21 18:30:24 -0500

    Add files via upload

--- rule110Stats.py
+++ rule110Stats.py
@@ -1,25 +1,22 @@
 from pprint import pprint
-def rule110CA(arr,steps):
+def rule110CA(arr):
     CARule = [0,1,1,0,1,1,1,0]
     N = len(arr)
-    arrX = [[10 for i in range(N)] for k in range(steps+1)]
+    arrX = [[10 for i in range(N)] for k in range(N)]
     arrX[0] = arr
-    s,L = 0,0
-    for k in range(1,steps+1):
-        for i in range(N):
+    for k in range(1,N):
+        arrX[k][0] = arrX[k-1][0]
+        arrX[k][N-1] = arrX[k-1][N-1]
+        for i in range(1,N-1):
             arrX[k][i] = CARule[
                 4*arrX[k-1][(i-1)%N]+2*arrX[k-1][i]+arrX[k-1][(i+1)%N]
                 ]
-    for i in range(len(arrX)): pprint(arrX[i])
-    if steps<=10:
-        print(0)
-        return
-    for k in range(10,steps+1):
-        for i in range(1,k+1):
+    for k in range(1,N-1):
+        s,L = 0,0
+        for i in range(N-1,N-k-2,-1):
             s,L = s + arrX[k][i], L+1
-    print(s/L)
-size = 10
-steps = 70
+        print(k, round(s/L,2))
+size = 200
 array = [0 for i in range(size-1)]
 array.append(1)
-rule110CA(array,steps)
+rule110CA(array)
